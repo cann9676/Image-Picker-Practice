95985f998a4e4d4615dd8d2bc92c62bd8d9383f8
cann9676 <80699111+cann9676@users.noreply.github.com>
2022-08-06 12:22:19 -0400

    Initial Commit

---
+++ Image Picker Practice/Image_Picker_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Image_Picker_PracticeApp.swift
+//  Image Picker Practice
+//
+//  Created by Chelsea Hannah on 8/6/22.
+//
+
+import SwiftUI
+
+@main
+struct Image_Picker_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
